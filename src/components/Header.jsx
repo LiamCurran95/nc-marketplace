b570b508157a7814f765180a5da1c3d5e92cb300
Samuel Parfait <samuelparfaitjr@gmail.com>
2022-03-04 11:04:09 +0000

    added guest user view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
+import { userContext } from "../context/UserContext";
+
 const Header = () => {
-	return (
-		<header className="Header">
-			<h1>NC Marketplace</h1>
-		</header>
-	);
+  const [user, setUser] = useState({
+    username: "Guest",
+    avatar_url:
+      "https://www.croptecshow.com/wp-content/uploads/2017/04/guest-avatar-250x250px.png",
+    kudos: 0,
+  });
+
+  return (
+    <userContext.Provider value={{ user, setUser }}>
+      <header className="Header">
+        <div></div>
+        <h1 className="logo">NC Marketplace</h1>
+        <div className="userInfo">
+          <img src={user.avatar_url} className="avatar" alt={user.username} />
+          <span className="username">{user.username}</span>
+        </div>
+      </header>
+    </userContext.Provider>
+  );
 };
 
 export default Header;
